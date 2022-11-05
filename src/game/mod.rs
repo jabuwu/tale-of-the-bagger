7b8bdfed66926585809a8b6b43345b4b4b830a2a
jabu <jabu@gx.ag>
2022-11-05 05:28:23 -0700

    fmt fix

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,21 +1,21 @@
 mod ambience;
 mod bag;
+mod container;
 mod conveyor;
 mod customer;
 mod depths;
 mod desk;
 mod game;
 mod product;
-mod container;
 mod products;
 
 pub use ambience::*;
 pub use bag::*;
+pub use container::*;
 pub use conveyor::*;
 pub use customer::*;
 pub use depths::*;
 pub use desk::*;
 pub use game::*;
 pub use product::*;
-pub use container::*;
 pub use products::*;
